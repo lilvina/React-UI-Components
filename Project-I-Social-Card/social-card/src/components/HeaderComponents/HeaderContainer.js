1fae57f79d42f5fa7f8d9fdde254aeb453c7598c
lilvina <davinaleo2007@yahoo.com>
2019-02-11 15:50:57 -0800

    mvp completed

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,20 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+import HeaderTitle from '.HeaderTitle';
+import ImageThumbnail from '.ImageThumbnail';
+
+function HeaderContainer() {
+  return (
+    <header>
+      <ImageThumbnail />
+      <div className="header-text">
+        <HeaderTitle />
+        <ImageThumbnail />
+      </div>
+    </header>
+
+  );
+}
+
+export default HeaderContainer;

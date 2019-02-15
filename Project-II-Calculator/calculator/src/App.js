65ca027501cf02613e063f058d944b6d237b8ced
lilvina <davinaleo2007@yahoo.com>
2019-02-14 22:46:41 -0800

    finally working on calculator

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,124 @@
 import React from 'react';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 import './App.css';
 
+const One = {
+  buttonName: 'btn-number',
+  text: '1'
+}
+
+const Two = {
+  buttonName: 'btn-number',
+  text: '2'
+}
+
+const Three = {
+  buttonName: 'btn-number',
+  text: '3'
+}
+
+const Four = {
+  buttonName: 'btn-number',
+  text: '4'
+}
+
+const Five = {
+  buttonName: 'btn-number',
+  text: '5'
+}
+const Six = {
+  buttonName: 'btn-number',
+  text: '6'
+}
+
+const Seven = {
+  buttonName: 'btn-number',
+  text: '7'
+}
+
+const Eight = {
+  buttonName: 'btn-number',
+  text: '8'
+}
+const Nine = {
+  buttonName: 'btn-number',
+  text: '9'
+}
+
+const Zero = {
+  buttonName: 'btn-number',
+  text: '0'
+}
+
+const Clear = {
+  buttonName: 'btn-action',
+  text: 'clear'
+}
+
+const AddButton = {
+  buttonName: "add",
+  text: "+"
+}
+
+const SubstractButton = {
+  buttonName: "substract",
+  text: "-"
+}
+
+const MultiplyButton = {
+  buttonName: "multiply",
+  text: "x"
+}
+
+const DivideButton = {
+  buttonName: "divide",
+  text: "/"
+}
+
+const EqualButton = {
+  buttonName: "equal",
+  text: "="
+}
+
+const DisplayButton = {
+  buttonName: "display",
+  text: "0"
+}
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+
+    <div className="container-calculator">
+      <div className="calculator one">
+        <CalculatorDisplay button={DisplayButton}/>
+      </div>
+      <div className="calculator two">
+        <ActionButton button={Clear}/>
+        <ActionButton button={DivideButton}/>
+      </div>
+      <div className="calculator three">
+        <NumberButton button={Seven} />
+        <NumberButton button={Eight} />
+        <NumberButton button={Nine} />
+        <ActionButton button={MultiplyButton} />
+      </div>
+      <div className="calculator four">
+        <NumberButton button={Four} />
+        <NumberButton button={Five} />
+        <NumberButton button={Six} />
+        <ActionButton button={SubstractButton} />
+      </div>
+      <div className="calculator five">
+        <NumberButton button={One} />
+        <NumberButton button={Two} />
+        <NumberButton button={Three} />
+        <ActionButton button={AddButton} />
+      </div>
+      <div className="calculator six">
+        <ActionButton button={Zero} />
+        <ActionButton button={EqualButton} />
+      </div>
     </div>
   );
 };
--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+
+
+const ActionButton = props => {
+  return (
+    <button className={props.button.buttonName}>{props.button.text}</button>
+  )
+}
+
+export default ActionButton
